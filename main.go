5e6209cfdd6491f841cbb9823d7e07f5dbf05088
bmanivas <bmanivas@in.ibm.com>
2019-05-16 12:11:58 +0530

    test

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	title := "Jenkins X golang http example tested by Bala"
+	title := "Jenkins X golang http example tested by Bala and Emin"
 
 	from := ""
 	if r.URL != nil {
